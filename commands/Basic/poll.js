eb52abd5a8b7f9eba7fab0c342d4d13fc32ab2e6
SiniMini876 <67224094+SiniMini876@users.noreply.github.com>
2020-06-30 23:32:57 +0300

    1

--- commands/Basic/poll.js
+++ commands/Basic/poll.js
@@ -2,15 +2,6 @@ const Discord = require('discord.js');
 const { Client, Util, MessageEmbed, MessageAttachment } = require("discord.js");
 
 exports.run = async(bot, message, args) => {
-    const Embd = new MessageEmbed()
-    .setColor(0x00BDFF)
-    .setTitle("מדריך הכנת סקרים בשרת נאד יפה")
-    .setDescription("וואלק תכתוב poll ואז את השאלה שאתה רוצה לשאול")
-
-    if(!args[1]){
-        message.author.send(Embd);
-        message.delete({ timeout: 5000 }).catch(console.error);
-    }
     if(args[1]){
         let msgArgs = args.slice(1).join(" ");
         message.channel.send("📋 " + "**" + msgArgs + "**").then(messageReaction => {
