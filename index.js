dcfd2c3f096ef0cade3e5ec6385b2a5c3681480b
SiniMini876 <67224094+SiniMini876@users.noreply.github.com>
2020-07-01 00:04:19 +0300

    1

--- index.js
+++ index.js
@@ -87,6 +87,11 @@ bot.on('message', async msg => {
       const poll = require('./commands/Basic/poll.js');
       poll.run(bot, msg, args)
       break;
+    case 'clear':
+      if(!msg.member.roles.cache.find(r => r.name === "OP")) return msg.channel.send('סורי אחי, אין לך רשות לעשות מעשה שכזה. יבומר');
+      if(!args[1]) return msg.channel.send('תגיד כמה הודעות למחוק יאפס')
+      msg.channel.bulkDelete(args[1]).catch(console.error);
+      break;
   }
   command = args.shift().toLowerCase();
 });
