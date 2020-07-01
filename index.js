7c98eeb0b98c7aa749dd3818554f7af5cb430733
SiniMini876 <67224094+SiniMini876@users.noreply.github.com>
2020-07-01 13:21:53 +0300

    1

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ bot.on('guildMemberAdd', member =>{
     const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
     if(!channel) return;
 
-    channel.send(`? ${mention.member}מה המצב`)
+    channel.send(`? ${member.mention}מה המצב`)
 
 });
 
