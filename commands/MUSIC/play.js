2ae2b40c237fc125b4420b631842544fecbb6a2f
SiniMini876 <67224094+SiniMini876@users.noreply.github.com>
2020-07-01 14:08:20 +0300

    1

--- commands/MUSIC/play.js
+++ commands/MUSIC/play.js
@@ -1,25 +1,10 @@
-const Discord = require("discord.js");
-const { Client, Util, MessageEmbed, MessageAttachment } = require("discord.js");
-const YouTube = require("simple-youtube-api");
-const ytdl = require("ytdl-core");
-const dotenv = require("dotenv").config();
-const TOKEN = process.env.BOT_TOKEN;
-const GOOGLE_API_KEY = process.env.YTAPI_KEY;
-const PREFIX = process.env.PREFIX;
-const cooldown = new Set();
-const youtube = new YouTube(GOOGLE_API_KEY);
-const queue = new Map();
-const bot = new Client({
-  disableMentions: "all"});
-const sr = require("./server.js");
+
 module.exports = {
     name: 'play',
     catagory: 'MUSIC  ',
     description: "PLAY",
     usage: "play",
-    run: async(bot, msg, args, Discord, YouTube, ytdl, dotenv, TOKEN, PREFIX, GOOGLE_API_KEY, cooldown, youtube, queue, sr) => { 
-
-
+    run: async(bot, msg, args, Discord, YouTube, ytdl, dotenv, TOKEN, PREFIX, GOOGLE_API_KEY, cooldown, youtube, queue, sr, searchString, url, serverQueue, cmd) => { 
 
     const voiceChannel = msg.member.voice.channel;
     if (!voiceChannel)
