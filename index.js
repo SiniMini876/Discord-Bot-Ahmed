ddb67362a276d7b6eae6c3f91ed59cb2aa32f909
SiniMini876 <67224094+SiniMini876@users.noreply.github.com>
2020-07-24 16:46:44 +0300

    Update

--- index.js
+++ index.js
@@ -55,34 +55,7 @@ bot.on('message', (msg) => {
   const words = require('./commands/Settings/custom_words.js');
   words.run(bot, msg, args)
 
-//switch (args[0]){
-//  case 'nod':
-//    const nod = require('./commands/Basic/nod_anak.js');
-//    nod.run(bot, msg, args, mc)
-//    break;
-//  case 'mute':
-//    const mute = require('./commands/Basic/mute.js');
-//    mute.run(bot, msg, args, ms)
-//    break;
-//  case 'rules':
-//    const rule = require('./commands/Basic/rules.js');
-//    rule.run(bot, msg, args)
-//    break;
-//  case 'poll':
-//    const poll = require('./commands/Basic/poll.js');
-//    poll.run(bot, msg, args)
-//    break;
-//  case 'help':
-//    const help = require('./commands/Basic/help.js');
-//    help.run(bot, msg, args)
-//    break;
-//  case 'clear`:
-//    if(!msg.member.roles.cache.find(r => r.name === "OP")) return msg.channel.send('סורי אחי, אין לך רשות לעשות מעשה שכזה. יבומר');
-//    if(!args[1]) return msg.channel.send('תגיד כמה הודעות למחוק יאפס')
-//    msg.channel.bulkDelete(args[1]).catch(console.error);
-//    break;
-//}
-//command = args.shift().toLowerCase();
+  command = args.shift().toLowerCase();
 });
 
 bot.login(PC_TOKEN);
