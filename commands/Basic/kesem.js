4d6267fad8681433e31d4108af551b475313810a
SiniMini876 <67224094+SiniMini876@users.noreply.github.com>
2020-07-25 21:22:28 +0300

    Made a new command : kun

---
+++ commands/Basic/kesem.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const { Client, Util, MessageEmbed, MessageAttachment } = require("discord.js");
+
+module.exports = class kun {
+    constructor(){
+        this.name = 'kun',
+        this.alias = ['kun'],
+        this.usage = '!kun'
+    }
+    run(bot, msg, args,){
+
+        if(!args[2]) return msg.reply('תשאל שאלה נורמלית בן אדם');
+        let replies = ['כן', 'לא', 'אני לא יודע, מה אני יעשה', 'תשאל מאוחר יותר אני נכה מידי כרגע'];
+
+        let result = Math.floor((Math.random() * replies.length));
+        let question = args.slice(1).join(' ');
+
+        let ballembed = new Discord.MessageEmbed()
+        .setTitle('קונכיית הקסם!')
+        .setFooter(msg.author.username)
+        .setColor('#FF9900')
+        .addField('שאלה', question)
+        .addField('תשובה', replies[result])
+        .setThumbnail('https://i.kym-cdn.com/photos/images/masonry/000/355/434/560.gif');
+        
+
+        msg.channel.send(ballembed);
+    }
+}
