6e7708ff1ff15dfcaeee832396d4cebbc8244845
SiniMini876 <yoavsinai22@gmail.com>
2021-01-31 11:40:47 +0200

    Updated - Added ALMO GUILD

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,3 +1,12 @@
 module.exports = (Discord, client, guildMember) => {
-    let welcomeChannel = guildMember.guild.channels.cache.get('719178323200180266').send(`מה המצב <@${guildMember.user.id}> ברוך הבא לשרת המגניב שלנו!`)
+    // ALMO
+    if(guildMember.guild.id === "693864294911049829"){
+        let welcomeChannel = guildMember.guild.channels.cache.get('719178323200180266').send(`מה המצב <@${guildMember.user.id}> ברוך הבא לשרת המגניב שלנו!`)
+    }
+    // Buganim
+    if(guildMember.guild.id === "805361587103203378"){
+        let welcomeChannel = guildMember.guild.channels.cache.get('805365025538965544').send(`שלום <@${guildMember.user.id}> אתה מהיום אלמו!`)
+        let almo = guildMember.guild.roles.cache.find(r => r.id === "805364807427162132");
+        guildMember.roles.add(almo)
+    }
 }
